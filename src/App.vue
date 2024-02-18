2a809099de0ccb6fabcd977d660871dfb7f544b5
agostinoluca <agostino.luca89@gmail.com>
2024-02-18 15:16:14 +0100

    add axios call

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <script>
+import axios from 'axios';
 export default {
   name: 'App',
   data() {
     return {
       inputText: '',
+      api_key: '71a27b20b786fe39560049b7c72c9d1f',
     }
   },
   methods: {
     startSearch() {
       console.log('Cerca', this.inputText);
-    }
+      this.getListMovies()
+    },
+    getListMovies() {
+      const words = this.inputText.split(' ');
+      const formattedInputText = words.join('+');
+      const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${formattedInputText}`;
+      console.log(apiUrl);
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    },
   }
 }
 </script>
