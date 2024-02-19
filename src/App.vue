bb90d07266e272edec17e35e4bea35e2a0b820cc
agostinoluca <agostino.luca89@gmail.com>
2024-02-19 19:48:46 +0100

    add v-for

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,24 @@ export default {
     return {
       inputText: '',
       api_key: '71a27b20b786fe39560049b7c72c9d1f',
+      movies: [],
     }
   },
   methods: {
     startSearch() {
-      console.log('Cerca', this.inputText);
+      // console.log('Cerca', this.inputText);
       this.getListMovies()
     },
     getListMovies() {
       const words = this.inputText.split(' ');
       const formattedInputText = words.join('+');
       const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${formattedInputText}`;
-      console.log(apiUrl);
+      // console.log(apiUrl);
       axios
         .get(apiUrl)
         .then((response) => {
-          console.log(response);
+          this.movies = response.data.results;
+          console.log(this.movies);
         })
         .catch((error) => {
           console.error(error);
@@ -34,6 +36,12 @@ export default {
 <template>
   <input v-model="inputText" @keyup.enter="startSearch" type="text" placeholder="Enter the title here">
   <button @click="startSearch">Search</button>
+  <ul v-for="movie in movies">
+    <li>{{ movie.title }}</li>
+    <li>{{ movie.original_title }}</li>
+    <li>{{ movie.original_language }}</li>
+    <li>{{ movie.vote_average }}</li>
+  </ul>
 </template>
 
 <style></style>
