8c4a7e212e5e67f7a1c8887e05071c140f0d145a
agostinoluca <agostino.luca89@gmail.com>
2024-02-20 19:56:48 +0100

    add API call for tv series

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,20 @@ export default {
     return {
       inputText: '',
       movies: [],
+      tvSeries: [],
       state
     }
   },
   methods: {
     startSearch() {
       // console.log('Cerca', this.inputText);
-      this.getListMovies()
+      this.getListMovies(),
+        this.getListSeries()
     },
     getListMovies() {
       const words = this.inputText.split(' ');
       const formattedInputText = words.join('+');
-      const apiUrl = `${state.api_url}${state.api_key}&query=${formattedInputText}`;
+      const apiUrl = `${state.movie_api_url}${state.api_key}&query=${formattedInputText}`;
       // console.log(apiUrl);
       axios
         .get(apiUrl)
@@ -30,18 +32,41 @@ export default {
           console.error(error);
         })
     },
+    getListSeries() {
+      const words = this.inputText.split(' ');
+      const formattedInputText = words.join('+');
+      const apiUrl = `${state.tv_series_api_url}${state.api_key}&query=${formattedInputText}`;
+      // console.log(apiUrl);
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          this.tvSeries = response.data.results;
+          // console.log(this.tvSeries);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    },
   }
 }
 </script>
 
 <template>
-  <input v-model="inputText" @keyup.enter="startSearch" type="text" placeholder="Enter the title here">
+  <input id="searchBar" v-model="inputText" @keyup.enter="startSearch" type="text" placeholder="Enter the title here">
   <button @click="startSearch">Search</button>
+  <h1>Movies</h1>
   <ul v-for="movie in movies">
     <li>{{ movie.title }}</li>
     <li>{{ movie.original_title }}</li>
     <li>{{ movie.original_language }}</li>
     <li>{{ movie.vote_average }}</li>
+  </ul>
+  <h1>Tv Series</h1>
+  <ul v-for="show in tvSeries">
+    <li>{{ show.name }}</li>
+    <li>{{ show.original_name }}</li>
+    <li>{{ show.original_language }}</li>
+    <li>{{ show.vote_average }}</li>
   </ul>
 </template>
 
--- src/state.js
+++ src/state.js
@@ -1,6 +1,7 @@
 import {reactive} from 'vue'
 
 export const state = reactive({
-    api_url: 'https://api.themoviedb.org/3/search/movie?api_key=',
+    movie_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=',
+    tv_series_api_url: 'https://api.themoviedb.org/3/search/tv?api_key=',
     api_key: '71a27b20b786fe39560049b7c72c9d1f',
 })
