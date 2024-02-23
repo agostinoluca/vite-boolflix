82ff038c7314865cd75db66f795bfbd4a3d156ce
agostinoluca <agostino.luca89@gmail.com>
2024-02-23 19:51:57 +0100

    add css utility add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,11 +16,22 @@ export default {
         
 <template>
     <header>
-        <input id="searchBar" v-model="inputText" @keyup.enter="emitInputText" type="text"
-            placeholder="Enter the title here">
-        <button @click="emitInputText">Search</button>
+        <div class="d-flex justify-content-between align-items-center p-2">
+            <div class="logo">
+                <img src="../assets/img/logo.png" alt="Logo Boolflix">
+            </div>
+            <div class="searchNav">
+                <input id="searchBar" v-model="inputText" @keyup.enter="emitInputText" type="text"
+                    placeholder="Enter the title here">
+                <button @click="emitInputText">Search</button>
+            </div>
+        </div>
     </header>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+header {
+    background-color: --boolflix-primary;
+}
+</style>
