e931302c222d7ea6e23fa80b3387e09c6082c846
agostinoluca <agostino.luca89@gmail.com>
2024-02-24 16:46:58 +0100

    add height to main add language fix country flag method

--- src/components/FlagCountry.vue
+++ src/components/FlagCountry.vue
@@ -6,33 +6,26 @@ export default {
     },
     methods: {
         flagCountryClass() {
-            if (this.originalLanguage === 'en' || this.originalLanguage === 'uk') {
-                return 'flag-icon flag-icon-gb';
-            } else if (this.originalLanguage === 'hi') {
-                return 'flag-icon flag-icon-us';
-            } else if (this.originalLanguage === 'ja') {
-                return 'flag-icon flag-icon-jp';
-            } else if (this.originalLanguage === 'te') {
-                return 'flag-icon flag-icon-in';
-            } else if (this.originalLanguage === 'sq') {
-                return 'flag-icon flag-icon-al';
-            } else if (this.originalLanguage === 'el') {
-                return 'flag-icon flag-icon-gr';
-            } else if (this.originalLanguage === 'zh') {
-                return 'flag-icon flag-icon-cn';
-            } else if (this.originalLanguage === 'ko') {
-                return 'flag-icon flag-icon-kr';
-            } else if (this.originalLanguage === 'cs') {
-                return 'flag-icon flag-icon-cz';
-            } else if (this.originalLanguage === 'ar' || this.originalLanguage === 'ur') {
-                return 'flag-icon flag-icon-ae';
-            } else if (this.originalLanguage === 'da') {
-                return 'flag-icon flag-icon-dk';
-            } else if (this.originalLanguage === 'ka') {
-                return 'flag-icon flag-icon-ge';
-            } else {
-                return 'flag-icon flag-icon-' + this.originalLanguage;
-            }
+            const languageMap = {
+                en: 'gb',
+                uk: 'gb',
+                hi: 'us',
+                ja: 'jp',
+                te: 'in',
+                sq: 'al',
+                el: 'gr',
+                zh: 'cn',
+                ko: 'kr',
+                cs: 'cz',
+                ar: 'ae',
+                ur: 'ae',
+                da: 'dk',
+                ka: 'ge',
+                he: 'il'
+            };
+
+            const countryCode = languageMap[this.originalLanguage] || this.originalLanguage;
+            return `flag-icon flag-icon-${countryCode}`;
         }
     },
 }
