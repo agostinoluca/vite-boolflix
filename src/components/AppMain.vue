5f102a4b930659b4f8642edb6d6d3b8a8d228df7
agostinoluca <agostino.luca89@gmail.com>
2024-02-25 14:14:33 +0100

    delete import FlagCountry in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,11 @@
 <script>
 import AppMovieCard from './AppMovieCard.vue';
 import AppTvSeriesCard from './AppTvSeriesCard.vue';
-import FlagCountry from './FlagCountry.vue';
 export default {
     name: 'AppMain',
     components: {
         AppMovieCard,
         AppTvSeriesCard,
-        FlagCountry
     },
 }
 </script>
