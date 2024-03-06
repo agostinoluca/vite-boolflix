c781757b891742bca072f236b379edc57b98fb89
agostinoluca <agostino.luca89@gmail.com>
2024-03-06 21:58:21 +0100

    fix selects

--- src/components/SelectGenresMovie.vue
+++ src/components/SelectGenresMovie.vue
@@ -11,13 +11,10 @@ export default {
 </script>
 
 <template>
-    <select class="genres_select" name="selectedGenre" id="selectedGenre" v-model="state.selectedGenreMovie">
+    <select class="genres_select" name="selectedGenreMovie" id="selectedGenreMovie" v-model="state.selectedGenreMovie">
         <option value="0">All Genre</option>
         <option v-for="genre in state.genresMovie" :value="genre.id">
             {{ genre.name }}
         </option>
     </select>
 </template>
-
-
-<style scoped></style>
--- src/components/SelectGenresTv.vue
+++ src/components/SelectGenresTv.vue
@@ -11,14 +11,10 @@ export default {
 </script>
 
 <template>
-    <select @change="console.log(state.tvSeries)" class="genres_select" name="selectedGenre" id="selectedGenre"
-        v-model="state.selectedGenreTv">
+    <select class="genres_select" name="selectedGenreTv" id="selectedGenreTv" v-model="state.selectedGenreTv">
         <option value="0">All Genre</option>
         <option v-for="genre in state.genresTv" :value="genre.id">
             {{ genre.name }}
         </option>
     </select>
 </template>
-
-
-<style scoped></style>
